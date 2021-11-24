6b427d634246f629227d97d0a054c6c47ff5d714
Abdus Salam <39504792+Salam-35@users.noreply.github.com>
2021-11-24 13:27:05 +0600

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public float followspeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=Vector3.Lerp(transform.position,player.position,Time.deltaTime*followspeed);
+    }
+}
